5e406369a3723fc922b95bbbf99d7ed10f67a81b
AntonioTrovato <72383399+AntonioTrovato@users.noreply.github.com>
2022-04-13 18:29:33 +0200

    Incident.py has been modified, I've added a "dump" method to convert the object status into a json string.

--- Incident.py
+++ Incident.py
@@ -1,18 +1,18 @@
 #This class represents a reported incident
 class Incident:
     
-    def __init__(self, incident_id, incident_name, control_id, control_name, estimated_time):
-        self.incident_id = incident_id
+    def __init__(self, threat_id, threat_name, control_id, control_name, estimated_time):
+        self.threat_id = threat_id
         self.control_id = control_id
-        self.incident_name = incident_name
+        self.threat_name = threat_name
         self.control_name = control_name
         self.estimated_time = estimated_time
     
-    def get_incident_id(self):
-        return self.incident_id
+    def get_threat_id(self):
+        return self.threat_id
     
-    def set_incident_id(self, incident_id):
-        self.incident_id = incident_id
+    def set_threat_id(self, threat_id):
+        self.threat_id = threat_id
     
     def get_control_id(self):
         return self.control_id
@@ -20,11 +20,11 @@ class Incident:
     def set_control_id(self, control_id):
         self.control_id = control_id
     
-    def get_incident_name(self):
-        return self.incident_name
+    def get_threat_name(self):
+        return self.threat_name
     
-    def set_incident_name(self, incident_name):
-        self.incident_name = incident_name
+    def set_threat_name(self, threat_name):
+        self.threat_name = threat_name
     
     def get_control_name(self):
         return self.control_name
@@ -38,6 +38,13 @@ class Incident:
     def set_estimated_time(self, estimated_time):
         self.estimated_time = estimated_time
 
+    def dump(self):
+        return {'threat_id': self.get_threat_id(),
+                'threat_name': self.get_threat_name(),
+                'control_id': self.get_control_id(),
+                'control_name': self.get_control_name(),
+                'estimated_time': self.get_estimated_time()}
+
     def __str__(self):
-        return "incident_id: " + str(self.get_incident_id()) + ", incident_name: " + self.get_incident_name() + ", control_id: " \
-               + str(self.get_control_id()) + ", control_name" + self.control_name + ", estimated_time: " + str(self.get_estimated_time())
+        return "threat_id: " + str(self.get_threat_id()) + ", threat_name: " + self.get_threat_name() + ", control_id: " \
+               + str(self.get_control_id()) + ", control_name: " + self.control_name + ", estimated_time: " + str(self.get_estimated_time())
